08727f5ffb36c511c37818ecb5ac66a8612b2832
GyuTae Kim <supxyrho@gmail.com>
2024-06-24 17:05:45 +0900

    Feat: 화면 출력 로직 추가

--- app.js
+++ app.js
@@ -4,24 +4,43 @@ const allUsers = require("./userInfos");
 const R = require("ramda");
 const dayjs = require("dayjs");
 
-const formatDate = (dateStr) =>
-  dayjs(dateStr, "YYYY-MM-DD:HH:mm").format("YYYY-MM-DD");
-
-const sortByAscDate = R.sortWith([R.ascend(R.prop("dateStr"))]);
-const uniqByDate = R.uniqBy(R.pipe(R.prop("dateStr"), formatDate));
+const {isMidnightTo2AM, sortByAscDate, uniqByDate, toEventScoreFormat} = require("./utils");
 
 const preprocessAllStudyRecords = R.pipe(
   R.groupBy(R.prop("userName")),
   R.values,
+  // @brief : Midnight ~ AM 2 사이 값을 이전날 23:59로 변경
+  R.map(
+    R.map(
+      R.when(
+        R.pipe(
+          R.prop('dateStr'),
+          isMidnightTo2AM
+        ),
+        R.evolve({
+          dateStr: (dateStr) => dayjs(dateStr, 'YYYY-MM-DD:HH:mm').subtract(1, "day").set('hour', 23).set('minute', 59).format("YYYY-MM-DD:HH:mm")
+        })
+      )
+    )
+  ),
+  // R.forEach(R.tap((el)=> console.log('el', el))),
   R.map(sortByAscDate),
-  R.map(uniqByDate)
+  R.map(uniqByDate),
+  R.map((record)=> {
+    const userName = R.pipe(R.head, R.prop("userName"))(record);
+    const eventJobName = R.pipe(R.find(R.propEq( userName, "userName")), R.prop('eventJobName'))(allUsers);
+    const user = R.find(R.propEq(userName, 'userName'))(allUsers);
+    return R.applySpec({
+      userName: R.always(userName),
+      eventJobName: R.always(eventJobName), 
+      totalPoint: user.calculateTotalPointsByRecords,
+      basePoint: user.calculateBasePointsByRecords,
+      bonusPoint: user.calculateBonusPointsByRecords
+    })(record);
+  }),
+  R.map(toEventScoreFormat),
+  R.join(' \n\n\n '),
+  R.tap(console.log)
 );
 
-const groupedStudyRecords = preprocessAllStudyRecords(allStudyRecords);
-
-console.log("hello world", groupedStudyRecords);
-
-console.log(
-  "groupedStudyRecords",
-  allUsers[0].calculateTotalPointsByRecords(groupedStudyRecords[0])
-);
+preprocessAllStudyRecords(allStudyRecords);
