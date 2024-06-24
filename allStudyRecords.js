b024146ac9795b4c1ac24b8816ff47fd4985adc2
GyuTae Kim <supxyrho@gmail.com>
2024-06-24 17:05:19 +0900

    Feat: 다른 Sample 아이템 추가

--- allStudyRecords.js
+++ allStudyRecords.js
@@ -30,6 +30,7 @@ module.exports = [
     type: "개발공부",
   },
 
+  // B
   {
     dateStr: "2024-10-27:10:27",
     userName: "B",
@@ -60,4 +61,22 @@ module.exports = [
     userName: "B",
     type: "개발공부",
   },
+
+  // C
+  {
+    dateStr: "2024-10-27 00:00",
+    userName: "C",
+    type: "개발공부",
+  },
+  {
+    dateStr: "2024-10-26 01:59",
+    userName: "C",
+    type: "개발공부",
+  },
+  {
+    dateStr: "2024-10-25 02:01",
+    userName: "C",
+    type: "개발공부",
+  },
+
 ];
