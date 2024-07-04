7b86b24ca913b2fba5dd69eaef24435c240ca623
GyuTaeKim <supxyrho@gmail.com>
2024-07-05 00:27:58 +0900

    Feat: 7월 1차 레코드 삽입

--- allStudyRecords.js
+++ allStudyRecords.js
@@ -1,81 +1,110 @@
 module.exports = [
   {
-    dateStr: "2024-10-27 10:27",
-    userName: "A",
-    type: "같이공부",
-  },
-  {
-    dateStr: "2024-10-25 10:26",
-    userName: "A",
-    type: "같이공부",
-  },
-  {
-    dateStr: "2024-10-25 10:25",
-    userName: "A",
-    type: "같이공부",
-  },
-  {
-    dateStr: "2024-10-27 10:27",
-    userName: "A",
-    type: "같이공부",
-  },
-  {
-    dateStr: "2024-10-26 10:26",
-    userName: "A",
+    dateStr: "2024-07-01 20:26",
+    userName: "최신영",
     type: "개발공부",
   },
   {
-    dateStr: "2024-10-25 10:25",
-    userName: "A",
-    type: "개발공부",
-  },
-
-  // B
-  {
-    dateStr: "2024-10-27 10:27",
-    userName: "B",
+    dateStr: "2024-07-02 13:14",
+    userName: "채문원",
     type: "개발공부",
   },
   {
-    dateStr: "2024-10-26 10:26",
-    userName: "B",
+    dateStr: "2024-07-02 20:45",
+    userName: "최신영",
     type: "개발공부",
   },
   {
-    dateStr: "2024-10-25 10:25",
-    userName: "B",
+    dateStr: "2024-07-03 20:29",
+    userName: "채문원",
     type: "개발공부",
   },
   {
-    dateStr: "2024-10-27 10:27",
-    userName: "B",
+    dateStr: "2024-07-03 21:49",
+    userName: "최신영",
     type: "개발공부",
   },
   {
-    dateStr: "2024-10-26 10:26",
-    userName: "B",
-    type: "개발공부",
-  },
-  {
-    dateStr: "2024-10-25 10:25",
-    userName: "B",
+    dateStr: "2024-07-04 23:51",
+    userName: "최신영",
     type: "개발공부",
   },
 
-  // C
-  {
-    dateStr: "2024-10-27 00:00",
-    userName: "C",
-    type: "개발공부",
-  },
-  {
-    dateStr: "2024-10-26 01:59",
-    userName: "C",
-    type: "개발공부",
-  },
-  {
-    dateStr: "2024-10-25 02:01",
-    userName: "C",
-    type: "개발공부",
-  },
+  // {
+  //   dateStr: "2024-10-27 10:27",
+  //   userName: "A",
+  //   type: "같이공부",
+  // },
+  // {
+  //   dateStr: "2024-10-25 10:26",
+  //   userName: "A",
+  //   type: "같이공부",
+  // },
+  // {
+  //   dateStr: "2024-10-25 10:25",
+  //   userName: "A",
+  //   type: "같이공부",
+  // },
+  // {
+  //   dateStr: "2024-10-27 10:27",
+  //   userName: "A",
+  //   type: "같이공부",
+  // },
+  // {
+  //   dateStr: "2024-10-26 10:26",
+  //   userName: "A",
+  //   type: "개발공부",
+  // },
+  // {
+  //   dateStr: "2024-10-25 10:25",
+  //   userName: "A",
+  //   type: "개발공부",
+  // },
+  // // B
+  // {
+  //   dateStr: "2024-10-27 10:27",
+  //   userName: "B",
+  //   type: "개발공부",
+  // },
+  // {
+  //   dateStr: "2024-10-26 10:26",
+  //   userName: "B",
+  //   type: "개발공부",
+  // },
+  // {
+  //   dateStr: "2024-10-25 10:25",
+  //   userName: "B",
+  //   type: "개발공부",
+  // },
+  // {
+  //   dateStr: "2024-10-27 10:27",
+  //   userName: "B",
+  //   type: "개발공부",
+  // },
+  // {
+  //   dateStr: "2024-10-26 10:26",
+  //   userName: "B",
+  //   type: "개발공부",
+  // },
+  // {
+  //   dateStr: "2024-10-25 10:25",
+  //   userName: "B",
+  //   type: "개발공부",
+  // },
+  // // C
+  // {
+  //   dateStr: "2024-10-27 00:00",
+  //   userName: "C",
+  //   type: "개발공부",
+  // },
+  // {
+  //   dateStr: "2024-10-26 01:59",
+  //   userName: "C",
+  //   type: "개발공부",
+  // },
+  // {
+  //   dateStr: "2024-10-25 02:01",
+  //   userName: "C",
+  //   type: "개발공부",
+  // },
 ];
