a38bb5c48575cfb72b4044407fb212b09cdc3d7e
GyuTae Kim <supxyrho@gmail.com>
2024-07-05 11:53:37 +0900

    Refactor: 로직 단순화

--- app.js
+++ app.js
@@ -20,14 +20,10 @@ const preprocess = R.curry((allUsers, allStudyRecords) =>
   R.pipe(
     R.groupBy(R.prop("userName")),
     R.values,
-    mapNested(
-      R.when(R.pipe(R.prop("dateStr"), isFrom1AMTill2AM), adjustToPrevDayEnd)
-    ),
     R.map(sortByAscDate),
-    R.map(uniqueByDay),
     R.map(
       R.converge(
-        (records, user) =>
+        (originalRecords, transformedRecords,  user) =>
           R.mergeAll([
             R.applySpec({
               userName: R.prop("userName"),
@@ -37,15 +33,15 @@ const preprocess = R.curry((allUsers, allStudyRecords) =>
             R.applySpec({
               totalScore: R.pipe(
                 R.prop("calculateTotalScoreByRecords"),
-                R.applyTo(R.__)(records)
+                R.applyTo(R.__)(transformedRecords)
               ),
               basePoint: R.pipe(
                 R.prop("calculateBasePointsByRecords"),
-                R.applyTo(R.__)(records)
+                R.applyTo(R.__)(transformedRecords)
               ),
               bonusPoint: R.pipe(
                 R.prop("calculateBonusPointsByRecords"),
-                R.applyTo(R.__)(records)
+                R.applyTo(R.__)(transformedRecords)
               ),
               scoreNeeded: R.converge(
                 R.pipe(R.subtract, R.clamp(0, Infinity)),
@@ -53,21 +49,24 @@ const preprocess = R.curry((allUsers, allStudyRecords) =>
                   R.prop("targetScore"),
                   R.pipe(
                     R.prop("calculateTotalScoreByRecords"),
-                    R.applyTo(R.__)(records)
+                    R.applyTo(R.__)(transformedRecords)
                   ),
                 ]
               ),
             })(user),
             R.applySpec({
-              lastStudyTime: lastStudyTime,
-              daySinceLastStudy: daySinceLastStudy,
-            })(records),
+              lastStudyTime,
+              daySinceLastStudy,
+            })(originalRecords),
           ]),
-
-        [
-          R.identity,
-          R.pipe(R.head, R.prop("userName"), R.flip(findUserByName)(allUsers)),
-        ]
+          [
+            R.identity,
+            R.pipe(
+              mapNested(R.when(R.pipe(R.prop("dateStr"), isFrom1AMTill2AM), adjustToPrevDayEnd)),
+              uniqueByDay
+            ),
+            R.pipe(R.head, R.prop("userName"), R.flip(findUserByName)(allUsers)),
+          ]
       )
     ),
     sortByDescTotalScore
