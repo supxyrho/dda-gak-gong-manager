052aee4f58f89552e051a87508ccdeb2372c0f97
GyuTae Kim <supxyrho@gmail.com>
2024-11-12 13:04:50 +0900

    Feat: 24.11.12일자 참가자 학습 기록

--- allStudyRecords.js
+++ allStudyRecords.js
@@ -17,7 +17,7 @@ module.exports = [
   {
     dateStr: "2024-11-03 17:36",
     userName: "고은민",
-    type: "개발공부",
+    type: "사이드프로젝트",
   },
 
   {
@@ -25,4 +25,157 @@ module.exports = [
     userName: "최신영",
     type: "개발공부",
   },
+
+  {
+    dateStr: "2024-11-05 21:58",
+    userName: "고은민",
+    type: "사이드프로젝트",
+  },
+
+  {
+    dateStr: "2024-11-05 21:59",
+    userName: "박진현",
+    type: "개발공부",
+  },
+
+  {
+    dateStr: "2024-11-05 22:20",
+    userName: "김규태",
+    type: "개발공부",
+  },
+
+  {
+    dateStr: "2024-11-06 11:22",
+    userName: "채문원",
+    type: "개발공부",
+  },
+
+  {
+    dateStr: "2024-11-07 13:17",
+    userName: "박상민",
+    type: "개발공부",
+  },
+
+  {
+    dateStr: "2024-11-07 13:24",
+    userName: "채문원",
+    type: "개발공부",
+  },
+
+  {
+    dateStr: "2024-11-07 21:46",
+    userName: "박진현",
+    type: "개발공부",
+  },
+
+  {
+    dateStr: "2024-11-07 21:56",
+    userName: "고은민",
+    type: "개발공부",
+  },
+
+  {
+    dateStr: "2024-11-07 22:50",
+    userName: "김승희",
+    type: "개발공부",
+  },
+
+  {
+    dateStr: "2024-11-08 16:08",
+    userName: "박진현",
+    type: "개발공부",
+  },
+
+  {
+    dateStr: "2024-11-08 17:10",
+    userName: "최신영",
+    type: "개발공부",
+  },
+
+  {
+    dateStr: "2024-11-08 19:00",
+    userName: "김규태",
+    type: "개발공부",
+  },
+
+  {
+    dateStr: "2024-11-08 23:30",
+    userName: "고은민",
+    type: "사이드프로젝트",
+  },
+
+  {
+    dateStr: "2024-11-09 12:36",
+    userName: "채문원",
+    type: "사이드프로젝트",
+  },
+
+  {
+    dateStr: "2024-11-09 22:38",
+    userName: "고은민",
+    type: "사이드프로젝트",
+  },
+
+  {
+    dateStr: "2024-11-10 01:59",
+    userName: "박상민",
+    type: "오픈소스",
+  },
+
+  {
+    dateStr: "2024-11-10 22:53",
+    userName: "박상민",
+    type: "오픈소스",
+  },
+
+  {
+    dateStr: "2024-11-10 16:28",
+    userName: "이한나",
+    type: "개발공부",
+  },
+
+  {
+    dateStr: "2024-11-10 16:46",
+    userName: "채문원",
+    type: "개발공부",
+  },
+
+  {
+    dateStr: "2024-11-10 22:53",
+    userName: "박상민",
+    type: "개발공부",
+  },
+
+  {
+    dateStr: "2024-11-10 23:45",
+    userName: "김승희",
+    type: "개발공부",
+  },
+
+  {
+    dateStr: "2024-11-11 20:00",
+    userName: "김규태",
+    type: "같이공부",
+  },
+
+  {
+    dateStr: "2024-11-11 20:00",
+    userName: "김승희",
+    type: "같이공부",
+  },
+
+  {
+    dateStr: "2024-11-11 20:00",
+    userName: "이한나",
+    type: "같이공부",
+  },
+
+  {
+    dateStr: "2024-11-11 20:00",
+    userName: "고은민",
+    type: "같이공부",
+  },
+
+  
+
 ];
